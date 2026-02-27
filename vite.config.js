0669094ad7f830a29eb644f7aefc38cde0579de2
Ernest rewer <s70239176@gmail.com>
2026-02-27 22:09:00 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  root: '.',
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: './index.html',
+    },
   },
   server: {
     port: 3000,
